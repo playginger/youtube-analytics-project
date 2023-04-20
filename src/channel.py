299a4a055f0ffd416ac5e03361f0e74014e54540
Серж Рыж <126515806+playginger@users.noreply.github.com>
2023-04-21 01:29:23 +0300

    commit

--- src/channel.py
+++ src/channel.py
@@ -1,11 +1,13 @@
 import requests
 import os
 
+
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        self.channel_info = None
         self.channel_id = channel_id
 
     def print_info(self) -> None:

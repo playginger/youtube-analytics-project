7960ae4e14714d6eba7f0e2e944cc3c67a64a53e
Серж Рыж <126515806+playginger@users.noreply.github.com>
2023-04-24 00:15:48 +0300

    commit

--- src/channel.py
+++ src/channel.py
@@ -7,6 +7,9 @@ import json
 class Channel:
     """Класс для ютуб-канала"""
 
+    api_key: str = os.getenv('YouTube API')
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
     def __init__(self, channel_id: str, info: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
 
